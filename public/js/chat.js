43e1f62cee1503b256b48852a60283d3889fe845
Rina Dishli <dishli271113@gmail.com>
2021-02-28 18:48:40 +0200

    added acknowledgement of delivering message and timestamp

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,62 @@
+/* eslint-disable no-undef */
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
+
 socket.on('message', (message) => {
-  document.querySelector('#message-1').textContent = message;
+//   document.querySelector('#message-1').textContent = message;
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  // disable form
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
-  socket.emit('sendMessage', message);
-});
+  socket.emit('sendMessage', message, (error) => {
+    // enable form
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus(); // cursor
 
-document.querySelector('#send-location').addEventListener('click', () => {
-  if (!navigator.geolocation) return alert('Geolocation is not supported by your browser');
-
-  navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit('sendLocation', `${position.coords.latitude},${position.coords.longitude}`);
+    if (error) return console.log(error);
+    return console.log('Delivered!');
   });
 });
 
-socket.on('sendLocation', (location) => {
-  document.querySelector('#message-2').textContent = location;
+$sendLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) return alert('Geolocation is not supported by your browser');
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
+  return navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', `${position.coords.latitude},${position.coords.longitude}`, (error) => {
+      $sendLocationButton.removeAttribute('disabled');
+
+      if (error) return console.log(error);
+      return console.log('Location Shared!');
+    });
+  });
+});
+
+socket.on('sendLocation', (message) => {
+  const html = Mustache.render(locationTemplate, {
+    locationUrl: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
+//   document.querySelector('#message-2').textContent = location;
 });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,19 +17,25 @@ app.use(express.json());
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.emit('message', 'Welcome!');
-  socket.broadcast.emit('message', 'A new user has joined!');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
-  socket.on('sendMessage', (message) => {
-    io.emit('message', message);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) { return callback('Profanity is not allowed'); }
+
+    io.emit('message', generateMessage(message));
+    return callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!');
+    io.emit('message', generateMessage('A user has left!'));
   });
 
-  socket.on('sendLocation', (location) => {
-    io.emit('sendLocation', `https://google.com/maps?q=${location}`);
+  socket.on('sendLocation', (location, callback) => {
+    io.emit('sendLocation', generateLocationMessage(`https://google.com/maps?q=${location}`));
+    callback();
   });
 });
 
